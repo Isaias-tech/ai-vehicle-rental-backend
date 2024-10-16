f35786c6c125a560a9e804469e5dfce6a1b293a2
Isaias-tech <isaias-santiago-toribio@outlook.com>
2024-10-16 13:56:32 -0400

    Fix: add statics to memory

--- vehicles_rental/urls.py
+++ vehicles_rental/urls.py
@@ -10,5 +10,4 @@ urlpatterns = [
     path("api/reservations/", include("reservations.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
